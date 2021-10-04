000bdaa1c03a149d530c25131ed43a6c8f4eacec
Panos Bozas <fbozas@hotmail.com>
2021-10-04 17:22:49 +0300

    Add project files.

---
+++ ActionResultsDemo/Controllers/ActionResultsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionResultsDemo.Controllers
+{
+    public class ActionResultsController : Controller
+    {
+        // GET: ActionResults
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult ViewResultMethod()
+        {
+            return View();
+        }
+
+        public ContentResult ContentMethod()
+        {
+            return Content("Hello from MVC 5");
+        }
+
+        public ActionResult ConditionalMethod()
+        {
+            var a = false;
+            if (a)
+            {
+                return Content("Variable is true");
+            }
+            return View();
+        }
+
+        public RedirectResult GoToUrl()
+        {
+            return Redirect("http://www.google.com");
+        }
+    }
+}
---
+++ ActionResultsDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ActionResultsDemo.MvcApplication" Language="C#" %>
